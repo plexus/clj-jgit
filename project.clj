c5423d6b67608a762111482a71889779b40e185f
Arne Brasseur <arne@arnebrasseur.net>
2016-02-02 06:34:33 +0100

    Update dependencies, add nop-logger
    Update dependencies, in particular JGit. The Java7 pack for JGit no longer
    exists, since it now always requires Java7.
    
    To prevent noisy warnings from Log4j, add a nop-logger in development mode.
    
    Bump version to 0.8.9-preview, to test if I can pull this in through JitPack.io.

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject clj-jgit "0.8.8"
+(defproject clj-jgit "0.8.9-preview"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit/org.eclipse.jgit.java7 "3.7.0.201502260915-r" :exclusions [com.jcraft/jsch]]
-                 [fs "1.3.2"]
-                 [com.jcraft/jsch "0.1.52"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [midje "1.5.1"]
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "4.2.0.201601211800-r"]
+                 [fs "1.3.3"]
+                 [com.jcraft/jsch "0.1.53"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+                                  [midje "1.8.3"]
                                   [com.stuartsierra/lazytest "1.2.3"]
-                                  [lein-clojars "0.9.0"]]} }
+                                  [lein-clojars "0.9.1"]
+                                  [org.slf4j/slf4j-nop "1.7.14"]]} }
   :plugins [[lein-midje "3.0.1"]
             [lein-marginalia "0.7.1"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
